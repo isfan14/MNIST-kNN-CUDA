3bd956bc29646b92bffd0844ab00e5be179af53a
Isfan Nur Fauzi <isfannurfauzi@gmail.com>
2024-11-24 08:23:04 +0000

    perf: reorder memory allocation to improve efficiency
    Allocate memory just before usage and free memory right after the last data.

--- src/main.cu
+++ src/main.cu
@@ -31,50 +31,74 @@ int main(int argc, char *argv[])
   char *h_train_labels, *h_test_labels, *d_train_labels, *d_test_labels;
   float *h_test_distances, *d_test_distances;
 
+  // allocate host memory to hold mnist data
   cudaMallocHost(&h_train_images_pixels, N_TRAIN * IMAGE_L * IMAGE_W * sizeof(int));
   cudaMallocHost(&h_test_images_pixels, N_TEST * IMAGE_L * IMAGE_W * sizeof(int));
   cudaMallocHost(&h_train_labels, N_TRAIN * sizeof(char));
   cudaMallocHost(&h_test_labels, N_TEST * sizeof(char));
-  cudaMallocHost(&h_test_distances, N_TEST * N_TRAIN * sizeof(float));
 
+  // load mnist train data to host
+  std::cout << "loading train data..." << std::endl;
+  read_mnist("mnist/train-images.idx3-ubyte", "mnist/train-labels.idx1-ubyte", h_train_images_pixels, h_train_labels);
+  std::cout << std::endl;
+
+  // load mnist test data to host
+  std::cout << "loading test data..." << std::endl;
+  read_mnist("mnist/t10k-images.idx3-ubyte", "mnist/t10k-labels.idx1-ubyte", h_train_images_pixels, h_test_labels);
+
+  // allocate device memory to hold mnist data
   cudaMalloc(&d_train_images_pixels, N_TRAIN * IMAGE_L * IMAGE_W * sizeof(int));
   cudaMalloc(&d_test_images_pixels, N_TEST * IMAGE_L * IMAGE_W * sizeof(int));
   cudaMalloc(&d_train_labels, N_TRAIN * sizeof(char));
   cudaMalloc(&d_test_labels, N_TEST * sizeof(char));
-  cudaMalloc(&d_test_distances, N_TEST * N_TRAIN * sizeof(float));
 
-  std::cout << "reading train data..." << std::endl;
-  read_mnist("mnist/train-images.idx3-ubyte", "mnist/train-labels.idx1-ubyte", h_train_images_pixels, h_train_labels);
-  std::cout << std::endl;
-
-  std::cout << "reading test data..." << std::endl;
-  read_mnist("mnist/t10k-images.idx3-ubyte", "mnist/t10k-labels.idx1-ubyte", h_train_images_pixels, h_test_labels);
-
+  // copy loaded host mnist data to device memory
   cudaMemcpy(d_train_images_pixels, d_train_images_pixels, sizeof(*d_train_images_pixels), cudaMemcpyHostToDevice);
   cudaMemcpy(d_train_labels, d_train_labels, sizeof(*d_train_labels), cudaMemcpyHostToDevice);
-
   cudaMemcpy(d_test_images_pixels, d_test_images_pixels, sizeof(*d_test_images_pixels), cudaMemcpyHostToDevice);
   cudaMemcpy(d_test_labels, d_test_labels, sizeof(*d_test_labels), cudaMemcpyHostToDevice);
 
-  int numBlocks = N_TRAIN;
-  dim3 threadsPerBlock(IMAGE_L, IMAGE_W);
+  // free mnist data from host memory
+  cudaFreeHost(&h_train_images_pixels);
+  cudaFreeHost(&h_test_images_pixels);
+  cudaFreeHost(&h_train_labels);
+  cudaFreeHost(&h_test_labels);
 
+  // allocate device memory to hold distances data
+  cudaMalloc(&d_test_distances, N_TEST * N_TRAIN * sizeof(float));
+
+  // define number of blocks
+  // each block handle 1 train image
+  int numBlocks = N_TRAIN;
+
+  // define number of threads per block
+  // using 2d threads correspond to each pixel in a train image
+  // each thread handle 1 pixel of distance calculation
+  dim3 threadsPerBlock = dim3(IMAGE_L, IMAGE_W);
+
+  // loop through all test data to calculate the distance
   for (int i = 0; i < N_TEST; i++)
   {
     GetPixelDistance<<<numBlocks, threadsPerBlock>>>(d_train_images_pixels, d_test_images_pixels, d_test_distances, i);
   }
 
-  cudaMemcpy(h_test_distances, d_test_distances, sizeof(d_test_distances), cudaMemcpyDeviceToHost);
-
-  cudaFreeHost(&h_train_images_pixels);
-  cudaFreeHost(&h_test_images_pixels);
-  cudaFreeHost(&h_train_labels);
-  cudaFreeHost(&h_test_labels);
-  cudaFreeHost(&h_test_distances);
-
+  // free mnist data from device memory
   cudaFree(&d_train_images_pixels);
   cudaFree(&d_test_images_pixels);
   cudaFree(&d_train_labels);
   cudaFree(&d_test_labels);
+
+  // allocate host memory to hold distances data
+  cudaMallocHost(&h_test_distances, N_TEST * N_TRAIN * sizeof(float));
+
+  // copy calculated distances data from device to host memory
+  cudaMemcpy(h_test_distances, d_test_distances, sizeof(d_test_distances), cudaMemcpyDeviceToHost);
+
+  // free distances data from device memory
   cudaFree(&d_test_distances);
+
+  // TODO: get inferred label based on the calculated distances
+
+  // free distances data from host memory
+  cudaFreeHost(&h_test_distances);
 }
