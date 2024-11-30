c125089e4f38a360858fb443035009ceb652c155
Isfan Nur Fauzi <isfannurfauzi@gmail.com>
2024-11-30 11:57:03 +0000

    feat: use pitched malloc

--- src/mnist/main.cu
+++ src/mnist/main.cu
@@ -3,16 +3,17 @@
 #include "../utils/mnist.hpp"
 #include "../utils/helper.cuh"
 
-__global__ void compute_diff(int *train_images, int *test_images, float *diffs, int test_idx)
+__global__ void compute_diff(int *train_images, int *test_images, size_t train_pitch, size_t test_pitch, float *diffs, size_t test_idx)
 {
   int train_idx = blockIdx.x;
   int pixel_idx = threadIdx.x;
 
-  int train_pixel_idx = train_idx * IMAGE_SIZE + pixel_idx;
-  int test_pixel_idx = test_idx * IMAGE_SIZE + pixel_idx;
+  int *train_image = (int *)((char *)train_images + train_idx * train_pitch);
+  int *test_image = (int *)((char *)test_images + +test_idx * test_pitch);
+
   int diff_idx = train_idx * IMAGE_SIZE + pixel_idx;
 
-  int diff = train_images[train_pixel_idx] - test_images[test_pixel_idx];
+  int diff = train_image[pixel_idx] - test_image[pixel_idx];
 
   diffs[diff_idx] = diff * diff;
 }
@@ -42,60 +43,51 @@ const size_t DISTANCES_SIZE = TRAIN_SIZE * sizeof(float);
 
 int main(int argc, char *argv[])
 {
-  int *h_train_images_pixels, *h_test_images_pixels, *d_train_images_pixels, *d_test_images_pixels;
+  int *h_train_images, *h_test_images, *d_train_images, *d_test_images;
   int *h_train_labels, *h_test_labels;
 
+  size_t train_pitch, test_pitch;
+
+  float *d_diffs, *d_distances, *h_distances;
+
+  size_t best_idx;
+  float best_distance;
+
+  int label;
+  int prediction;
+  size_t true_prediction = 0;
+
+  float elapsed_time_ms;
+
   // allocate host memory to hold mnist data
-  cudaMallocHost((void **)&h_train_images_pixels, TRAIN_IMAGES_SIZE);
+  cudaMallocHost((void **)&h_train_images, TRAIN_IMAGES_SIZE);
   cudaMallocHost((void **)&h_train_labels, TRAIN_LABELS_SIZE);
-  cudaMallocHost((void **)&h_test_images_pixels, TEST_IMAGES_SIZE);
+  cudaMallocHost((void **)&h_test_images, TEST_IMAGES_SIZE);
   cudaMallocHost((void **)&h_test_labels, TEST_LABELS_SIZE);
 
   // load mnist train data to host
   std::cout << "loading train data..." << std::endl;
-  read_mnist("mnist/train-images.idx3-ubyte", "mnist/train-labels.idx1-ubyte", h_train_images_pixels, h_train_labels);
+  read_mnist("mnist/train-images.idx3-ubyte", "mnist/train-labels.idx1-ubyte", h_train_images, h_train_labels);
   std::cout << std::endl;
 
   // load mnist test data to host
   std::cout << "loading test data..." << std::endl;
-  read_mnist("mnist/t10k-images.idx3-ubyte", "mnist/t10k-labels.idx1-ubyte", h_test_images_pixels, h_test_labels);
+  read_mnist("mnist/t10k-images.idx3-ubyte", "mnist/t10k-labels.idx1-ubyte", h_test_images, h_test_labels);
 
-  // allocate device memory to hold mnist data
-  cudaMalloc((void **)&d_train_images_pixels, TRAIN_IMAGES_SIZE);
-  cudaMalloc((void **)&d_test_images_pixels, TEST_IMAGES_SIZE);
+  // load mnist test data to device
+  cudaMallocPitch(&d_train_images, &train_pitch, IMAGE_SIZE * sizeof(int), TRAIN_SIZE);
+  cudaMemcpy2D(d_train_images, train_pitch, h_train_images, IMAGE_SIZE * sizeof(int), IMAGE_SIZE * sizeof(int), TRAIN_SIZE, cudaMemcpyHostToDevice);
+  cudaFreeHost(h_train_images);
 
-  // copy loaded host mnist data to device memory
-  cudaMemcpy(d_train_images_pixels, h_train_images_pixels, TRAIN_IMAGES_SIZE, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_test_images_pixels, h_test_images_pixels, TEST_IMAGES_SIZE, cudaMemcpyHostToDevice);
-
-  // free mnist data from host memory
-  cudaFreeHost(h_train_images_pixels);
-  cudaFreeHost(h_test_images_pixels);
-
-  float *d_diffs, *d_distances, *h_distances;
+  // load mnist test data to device
+  cudaMallocPitch(&d_test_images, &test_pitch, IMAGE_SIZE * sizeof(int), TEST_SIZE);
+  cudaMemcpy2D(d_test_images, test_pitch, h_test_images, IMAGE_SIZE * sizeof(int), IMAGE_SIZE * sizeof(int), TEST_SIZE, cudaMemcpyHostToDevice);
+  cudaFreeHost(h_test_images);
 
   cudaMalloc((void **)&d_diffs, DIFFS_SIZE);
   cudaMalloc((void **)&d_distances, DISTANCES_SIZE);
 
   cudaMallocHost((void **)&h_distances, DISTANCES_SIZE);
-
-  unsigned int true_prediction = 0;
-  unsigned int best_idx;
-  float best_distance;
-
-  int label;
-  int prediction;
-
-  // define number of blocks
-  // each block handle 1 train image
-  int numBlocks = TRAIN_SIZE;
-
-  // define number of threads per block
-  // using 2d threads correspond to each pixel in a train image
-  // each thread handle 1 pixel of distance calculation
-  int threadsPerBlock = IMAGE_SIZE;
-
-  float elapsed_time_ms;
 
   // some events to count the execution time
   cudaEvent_t start, stop;
@@ -106,26 +98,18 @@ int main(int argc, char *argv[])
   cudaEventRecord(start, 0);
 
   // loop through all test data to calculate the distance
-  for (int test_idx = 0; test_idx < TEST_SIZE; test_idx++)
+  for (size_t test_idx = 0; test_idx < TEST_SIZE; test_idx++)
   {
     // 1. compute euclidean distance (distance = sqrt(diff))
     // 1.1. compute diff (inside of sqrt)
-    compute_diff<<<numBlocks, threadsPerBlock>>>(d_train_images_pixels, d_test_images_pixels, d_diffs, test_idx);
-    CUDACHECK(cudaPeekAtLastError());
-
-    // cudaDeviceSynchronize();
+    compute_diff<<<TRAIN_SIZE, IMAGE_SIZE>>>(d_train_images, d_test_images, train_pitch, test_pitch, d_diffs, test_idx);
     // CUDACHECK(cudaPeekAtLastError());
 
     // 1.2. compute distance
-    // compute_distance_atomic<<<numBlocks, IMAGE_SIZE>>>(d_diffs, d_distances, test_idx);
-    compute_distance<<<numBlocks, 1>>>(d_diffs, d_distances, test_idx);
-    CUDACHECK(cudaPeekAtLastError());
-
-    // cudaDeviceSynchronize();
+    compute_distance<<<TRAIN_SIZE, 1>>>(d_diffs, d_distances, test_idx);
     // CUDACHECK(cudaPeekAtLastError());
 
     cudaMemcpy(h_distances, d_distances, DISTANCES_SIZE, cudaMemcpyDeviceToHost);
-    CUDACHECK(cudaPeekAtLastError());
 
     // 2. find the closest train image to the current test image
     best_idx = 0;
@@ -170,8 +154,8 @@ int main(int argc, char *argv[])
   std::cout << "true predictions: " << true_prediction << " percentage: " << percentage << " elapsed: " << elapsed_time_ms << std::endl;
 
   // free mnist data from device memory
-  cudaFree(d_train_images_pixels);
-  cudaFree(d_test_images_pixels);
+  cudaFree(d_train_images);
+  cudaFree(d_test_images);
 
   // free distances data from host memory
   cudaFreeHost(h_train_labels);
